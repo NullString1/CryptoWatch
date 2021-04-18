26b67c0c4f64e3d0d15c54884faa440df88a3756
NullString <53035336+NullString1@users.noreply.github.com>
2021-04-18 09:14:20 +0100

    Code cleanup and bug fix
    - Now shows negative price changes
    - Fixed pointless repeat of code
    - Fixed XPATH

--- cyptowatch.py
+++ cyptowatch.py
@@ -10,14 +10,15 @@ curs = []
 
 
 class cur:
-
     r = None
     data = None
     price = None
     inc = None
     name = None
+    text = None
 
     def __init__(self, url, name):
+        self.text = []
         self.r = requests.get(url, headers={"content-encoding": "gzip"},
                               stream=True)
         self.r.raw.decode_content = True
@@ -26,8 +27,14 @@ class cur:
                             " priceTitle___1cXUG')]/*"))
         self.price = "$" + str(round(float(self.data[0].text[1:]
                                            .replace(",", "")), 4))
-        self.inc = self.data[1].text_content().replace("(", "").replace(")", "")
+        self.inc = self.data[1].text_content().replace("(",
+                                                       "").replace(")", "")
+        style = self.data[1].attrib.get("style")
+        if style[style.find(";color:")+8:style.find(";padding")] == "fff":
+            self.inc = "-"+self.inc
         self.name = name
+        self.text.append(f"PRC ({self.name}): {self.price[:8]}")
+        self.text.append(f"INC: {self.inc}")
 
 
 curs.append(cur("https://coinmarketcap.com/currencies/basic-attention-token",
@@ -37,35 +44,22 @@ curs.append(cur("https://coinmarketcap.com/currencies/ethereum", "ETH"))
 curs.append(cur("https://coinmarketcap.com/currencies/bitcoin", "BTC"))
 
 path = os.environ["LOCALAPPDATA"]+"\\original.png"
-if os.path.isfile(path):
-    base = Image.open(path).convert("RGBA")
-    txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
-    fnt = ImageFont.truetype('C:\\Windows\\Fonts\\Arial.ttf', 18)
-    d = ImageDraw.Draw(txt)
-    for c in range(len(curs)):
-        d.text((1740, 954-c*60), f"PRC ({curs[c].name}): {curs[c].price[:8]}",
-               font=fnt, fill=(255, 255, 255, 255))
-        d.text((1740, 984-c*60), f"INC: {curs[c].inc}", font=fnt,
-               fill=(255, 255, 255, 255))
-    out = Image.alpha_composite(base, txt)
-    out.save(os.environ["LOCALAPPDATA"]+"\\Temp\\back.png")
-    ctypes.windll.user32.SystemParametersInfoW(0x14, 0,
-                                               os.environ["LOCALAPPDATA"] +
-                                               "\\Temp\\back.png", 0)
-else:
+
+if not os.path.isfile(path):
     shutil.copy2(os.environ["APPDATA"]+"\\Microsoft\\Windows\\"
                  "Themes\\CachedFiles\\CachedImage_1920_1080_POS4.jpg", path)
-    base = Image.open(path).convert("RGBA")
-    txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
-    fnt = ImageFont.truetype('C:\\Windows\\Fonts\\Arial.ttf', 20)
-    d = ImageDraw.Draw(txt)
-    for c in range(len(curs)):
-        d.text((1740, 954-c*60), f"PRC ({curs[c].name}): {curs[c].price[:8]}",
-               font=fnt, fill=(255, 255, 255, 255))
-        d.text((1740, 984-c*60), f"INC: {curs[c].inc}", font=fnt,
-               fill=(255, 255, 255, 255))
-    out = Image.alpha_composite(base, txt)
-    out.save(os.environ["APPDATA"]+"\\Temp\\back.png")
-    ctypes.windll.user32.SystemParametersInfoW(0x14, 0,(
-                                               os.environ["LOCALAPPDATA"] +
-                                               "\\Temp\\back.png"), 0)
+
+base = Image.open(path).convert("RGBA")
+txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+fnt = ImageFont.truetype('C:\\Windows\\Fonts\\Arial.ttf', 18)
+d = ImageDraw.Draw(txt)
+for c in range(len(curs)):
+    d.text((1740, 954-c*60), curs[c].text[0],
+           font=fnt, fill=(255, 255, 255, 255))
+    d.text((1740, 984-c*60), curs[c].text[1], font=fnt,
+           fill=(255, 255, 255, 255))
+out = Image.alpha_composite(base, txt)
+out.save(os.environ["LOCALAPPDATA"]+"\\Temp\\back.png")
+ctypes.windll.user32.SystemParametersInfoW(0x14, 0, (
+                                           os.environ["LOCALAPPDATA"] +
+                                           "\\Temp\\back.png"), 0)
