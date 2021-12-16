dc30ada22cc4a5490f5bacbf9fbd0428a54c09c9
NullString <53035336+NullString1@users.noreply.github.com>
2021-12-16 19:08:37 +0000

    Add automatic task creation

--- cyptowatch.py
+++ cyptowatch.py
@@ -4,37 +4,45 @@ import ctypes
 import shutil
 import os.path
 import os
+import subprocess
+import sys
 from PIL import Image, ImageDraw, ImageFont
 
-apikey = ""
+
 currencies = ["BAT", "VET", "BTC", "ETH", "LUNA", "XVS", "XRP", "LINK", "ADA"]
-curs = []
+spacing = 5
+margin = (5, 200)
+
+if sys.argv[-1] != "-d":
+    if subprocess.Popen("schtasks.exe /Query /TN CryptoWatch".split(), stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW).wait():
+        if ctypes.windll.shell32.IsUserAnAdmin():
+            execpath = os.path.dirname(
+                sys.executable) + "\\pythonw.exe " + __file__ + " -d"
+            os.system(
+                f"schtasks.exe /Create /SC ONLOGON /TN CryptoWatch /TR \"{execpath}\" /IT")
+            os.system("echo.|schtasks.exe /Change /TN CryptoWatch /RI 5")
+        else:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", os.path.dirname(
+                sys.executable) + "\\python.exe", " ".join(sys.argv), None, 0)
 
 
 class currency:
-    r = None
-    price = None
-    inc = None
-    symbol = None
-    json = None
-    text = None
+    def __init__(self, symbol: str, resp: dict):
+        self.price: int = resp["data"][symbol]["quote"]["USD"]["price"]
+        self.inc: int = resp["data"][symbol]["quote"]["USD"]["percent_change_24h"]
+        self.text = f"{symbol}: {round(self.price, 2)} ({round(self.inc, 2)}%)"
 
-    def __init__(self, symbol, resp):
-        self.text = []
-        self.price = resp["data"][symbol]["quote"]["USD"]["price"]
-        self.inc = resp["data"][symbol]["quote"]["USD"]["percent_change_24h"]
-        self.text.append(f"PRC ({symbol}): {round(self.price, 2)}")
-        self.text.append(f"INC: {round(self.inc,2)}%")
 
+res = (ctypes.windll.user32.GetSystemMetrics(0),
+       ctypes.windll.user32.GetSystemMetrics(1))
+curs: list[currency] = []
 
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol="
-url += currencies.__repr__().replace("'", "").replace(" ", "")[1:-1]
-# for a in currencies:
-#    url += a+","
+url += ",".join(currencies)
 
 r = requests.get(
-    url, headers={"X-CMC_PRO_API_KEY": apikey})
-resp = json.loads(r.text)
+    url, headers={"X-CMC_PRO_API_KEY": "APIKEY"})
+resp: dict = json.loads(r.text)
 
 for a in currencies:
     curs.append(currency(a, resp))
@@ -47,13 +55,20 @@ if not os.path.isfile(path):
 
 base = Image.open(path).convert("RGBA")
 txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
-fnt = ImageFont.truetype('C:\\Windows\\Fonts\\Arial.ttf', 18)
+fnt = ImageFont.truetype('C:\\Windows\\Fonts\\Arial.ttf', 19)
 d = ImageDraw.Draw(txt)
+
 for c in range(len(curs)):
-    d.text((1740, 1014-c*60), curs[c].text[0],
-           font=fnt, fill=(255, 255, 255, 255))
-    d.text((1740, 1044-c*60), curs[c].text[1], font=fnt,
-           fill=(255, 255, 255, 255))
+    colour = (255, 255, 255, 255)
+
+    if curs[c].inc > 0:
+        colour = (22, 199, 132, 255)
+    else:
+        colour = (234, 57, 67, 255)
+    size = d.textsize(curs[c].text, fnt)
+    d.text((res[0]-margin[0]-size[0], res[1]-margin[1]-c*(size[1]+spacing)), curs[c].text, font=fnt,
+           fill=colour)
+
 out = Image.alpha_composite(base, txt)
 out.save(os.environ["LOCALAPPDATA"]+"\\Temp\\back.png")
 ctypes.windll.user32.SystemParametersInfoW(0x14, 0, (
